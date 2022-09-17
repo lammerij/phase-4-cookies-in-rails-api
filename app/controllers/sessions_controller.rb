e7bc2d5a3762bcae82829329d978d46ffbba6bbb
+ <+>
2022-09-17 16:18:56 -0400

    commit tree 96f8f13e917fd38f5a274f49b7c69adb26766720

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
   
   def index
     session[:session_hello] ||= "World"
-    cookies[:cookies_hello] ||= "World"
+    cookies[:cookies_heaven] ||= "Heaven"
     render json: { session: session, cookies: cookies.to_hash }
   end
 
